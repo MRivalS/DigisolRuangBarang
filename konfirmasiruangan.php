05873744bbbb13b63a755b02abed007a5a111ca5
Maspur102 <135262678+Maspur102@users.noreply.github.com>
2025-01-26 01:39:34 +0700

    rensponsif

--- konfirmasiruangan.php
+++ konfirmasiruangan.php
@@ -58,7 +58,7 @@ $nama = $_SESSION['nama'];
 
         <body>
           <h1 style="color: white; font-size: 24px; font-family: 'Poppins', sans-serif; margin: 0;">
-            Hello, <?php echo $nama?>, (<?php echo $role ?>)
+            Hello, <?php echo $nama ?>, (<?php echo $role ?>)
           </h1>
         </body>
       </div>
@@ -86,147 +86,182 @@ $nama = $_SESSION['nama'];
       </li>
     </ul>
   </nav>
-  <div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-        <div class="sb-sidenav-menu">
-          <div class="nav">
-            <div class="sb-sidenav-menu-heading">Home</div>
-            <a class="nav-link" href="user.php">
-              <div class="sb-nav-link-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-house-door"
-                  viewBox="0 0 16 16">
-                  <path
-                    d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
-                </svg>
-              </div>
-              Home
-            </a>
-            <div class="sb-sidenav-menu-heading">Layanan</div>
-
-            <a
-              class="nav-link collapsed"
-              href="#"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseLayouts1"
-              aria-expanded="false"
-              aria-controls="collapseLayouts1">
-              <div class="sb-nav-link-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
-                  <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5" />
-                  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
-                </svg>
-              </div>
-              PEMINJAMAN
-              <div class="sb-sidenav-collapse-arrow">
-                <i class="fas fa-angle-down"></i>
-              </div>
-            </a>
-            <div
-              class="collapse"
-              id="collapseLayouts1"
-              aria-labelledby="headingOne"
-              data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="pinjambarang.php">Pinjam Barang</a>
-                <a class="nav-link" href="pinjamruangan.php">Pinjam Ruangan</a>
-              </nav>
+  <div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark collapse" id="sidenavAccordion">
+      <div class="sb-sidenav-menu">
+        <div class="nav">
+          <div class="sb-sidenav-menu-heading">Home</div>
+          <a class="nav-link" href="user.php">
+            <div class="sb-nav-link-icon">
+              <i class="bi bi-house-door"></i>
             </div>
-
-
-
-
-            <a class="nav-link" href="konfirmasi.php">
-              <div class="sb-nav-link-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
-                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                  <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
-                </svg>
-              </div>
-              KONFIRMASI
-            </a>
-
-
-
-            <a
-              class="nav-link collapsed"
-              href="#"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseLayouts2"
-              aria-expanded="false"
-              aria-controls="collapseLayouts2">
-              <div class="sb-nav-link-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-minus" viewBox="0 0 16 16">
-                  <path d="M5.5 9a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5" />
-                  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
-                </svg>
-              </div>
-              PENGEMBALIAN
-              <div class="sb-sidenav-collapse-arrow">
-                <i class="fas fa-angle-down"></i>
-              </div>
-            </a>
-            <div
-              class="collapse"
-              id="collapseLayouts2"
-              aria-labelledby="headingTwo">
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="pengembalianbarang.php">Barang </a>
-                <a class="nav-link" href="pengembalianruangan.php"> Ruangan </a>
-              </nav>
-            </div>
-
-          </div>
+            Home
         </div>
-      </nav>
-    </div>
-    <div id="layoutSidenav_content">
-      <main>
-        <div class="container-fluid px-4">
-          <h1 class="mt-4">KONFIRMASI PEMINJAMAN RUANGAN</h1>
-          <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item"><a href="user.php">Home</a></li>
-            <li class="breadcrumb-item active">Data Konfirmasi Ruangan</li>
-          </ol>
-          <div class="card mb-4">
-            <div class="card-header">
-              <i class="fas fa-table me-1"></i>
-              View Konfirmasi Peminjaman Ruangan
-            </div>
-
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Nama Ruangan</th>
-                  <th scope="col">Tgl. pinjam</th>
-                  <th scope="col">Tgl. kembali</th>
-                  <th scope="col">Riwayat</th>
-                  <th scope="col">Status</th>
-                </tr>
-              </thead>
-            </table>
+        <a class="nav-link" href="user.php">
+          <div class="sb-nav-link-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-house-door"
+              viewBox="0 0 16 16">
+              <path
+                d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
+            </svg>
           </div>
+          Home
+        </a>
+        <div class="sb-sidenav-menu-heading">Layanan</div>
 
-
-      </main>
-      <footer class="py-4 bg-light mt-auto">
-        <div class="container-fluid px-4">
-          <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-
+        <a
+          class="nav-link collapsed"
+          href="#"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseLayouts1"
+          aria-expanded="false"
+          aria-controls="collapseLayouts1">
+          <div class="sb-nav-link-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
+              <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5" />
+              <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
+            </svg>
           </div>
+          PEMINJAMAN
+          <div class="sb-sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+          </div>
+        </a>
+        <div
+          class="collapse"
+          id="collapseLayouts1"
+          aria-labelledby="headingOne"
+          data-bs-parent="#sidenavAccordion">
+          <nav class="sb-sidenav-menu-nested nav">
+            <a class="nav-link" href="pinjambarang.php">Pinjam Barang</a>
+            <a class="nav-link" href="pinjamruangan.php">Pinjam Ruangan</a>
+          </nav>
         </div>
-      </footer>
-    </div>
+
+
+
+
+        <a class="nav-link" href="konfirmasi.php">
+          <div class="sb-nav-link-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+              <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
+            </svg>
+          </div>
+          KONFIRMASI
+        </a>
+
+
+
+        <a
+          class="nav-link collapsed"
+          href="#"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseLayouts2"
+          aria-expanded="false"
+          aria-controls="collapseLayouts2">
+          <div class="sb-nav-link-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-minus" viewBox="0 0 16 16">
+              <path d="M5.5 9a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5" />
+              <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
+            </svg>
+          </div>
+          PENGEMBALIAN
+          <div class="sb-sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+          </div>
+        </a>
+        <div
+          class="collapse"
+          id="collapseLayouts2"
+          aria-labelledby="headingTwo">
+          <nav class="sb-sidenav-menu-nested nav">
+            <a class="nav-link" href="pengembalianbarang.php">Barang </a>
+            <a class="nav-link" href="pengembalianruangan.php"> Ruangan </a>
+          </nav>
+        </div>
+
+      </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script src="js/scripts.js"></script>
+  </nav>
+  </div>
+  <div id="layoutSidenav_content">
+    <main>
+      <div class="container-fluid px-4">
+        <h1 class="mt-4">KONFIRMASI PEMINJAMAN RUANGAN</h1>
+        <ol class="breadcrumb mb-4">
+          <li class="breadcrumb-item"><a href="user.php">Home</a></li>
+          <li class="breadcrumb-item active">Data Konfirmasi Ruangan</li>
+        </ol>
+        <div id="layoutSidenav_content">
+          <main>
+            <div class="container-fluid px-4">
+              <h1 class="mt-4">KONFIRMASI PEMINJAMAN RUANGAN</h1>
+              <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="user.php">Home</a></li>
+                <li class="breadcrumb-item active">Data Konfirmasi Ruangan</li>
+              </ol>
+              <div class="card mb-4">
+                <div class="card-header">
+                  <i class="fas fa-table me-1"></i>
+                  View Konfirmasi Peminjaman Ruangan
+                </div>
+                <!-- Tabel responsif -->
+                <div class="table-responsive">
+                  <table class="table table-dark table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Ruangan</th>
+                        <th scope="col">Tgl. Pinjam</th>
+                        <th scope="col">Tgl. Kembali</th>
+                        <th scope="col">Riwayat</th>
+                        <th scope="col">Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <!-- PHP untuk data tabel -->
+                      <?php
+                      $no = 1;
+                      if ($confirmedResult->num_rows > 0) {
+                        while ($row = $confirmedResult->fetch_assoc()) {
+                          echo "<tr>
+                                <td>{$no}</td>
+                                <td>{$row['nama_ruangan']}</td>
+                                <td>{$row['tanggal_mulai']}</td>
+                                <td>{$row['tanggal_selesai']}</td>
+                                <td>{$row['riwayat']}</td>
+                                <td>{$row['status']}</td>
+                            </tr>";
+                          $no++;
+                        }
+                      } else {
+                        echo "<tr><td colspan='6'>Belum ada data konfirmasi.</td></tr>";
+                      }
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </main>
+          <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+              <div class="d-flex align-items-center justify-content-between small">
+                <div class="text-muted">Copyright &copy; Your Website 2023</div>
+
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+      <script src="js/scripts.js"></script>
 </body>
 
 </html>
