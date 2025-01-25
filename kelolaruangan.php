9969aa60cb1a912162b21584f5e8218544e072c5
Maspur102 <135262678+Maspur102@users.noreply.github.com>
2025-01-25 23:21:21 +0700

    nav kelola ruangan

--- kelolaruangan.php
+++ kelolaruangan.php
@@ -15,9 +15,6 @@ $nama = $_SESSION['nama'];
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -61,7 +58,7 @@ $nama = $_SESSION['nama'];
 
                 <body>
                     <h1 style="color: white; font-size: 24px; font-family: 'Poppins', sans-serif; margin: 0;">
-                        Hello, <?php echo $nama?>, (<?php echo $role ?>)
+                        Hello, <?php echo $nama ?>, (<?php echo $role ?>)
                     </h1>
                 </body>
             </div>
@@ -114,7 +111,7 @@ $nama = $_SESSION['nama'];
 
                         <a
                             class="nav-link collapsed"
-                            href="#"
+                            href=""
                             data-bs-toggle="collapse"
                             data-bs-target="#collapseLayouts1"
                             aria-expanded="false"
@@ -138,6 +135,7 @@ $nama = $_SESSION['nama'];
                             <nav class="sb-sidenav-menu-nested nav">
                                 <a class="nav-link" href="kelolabarang.php">Data Barang</a>
                                 <a class="nav-link" href="kelolaruangan.php">Data Ruangan</a>
+                                <a class="nav-link" href="tambahan_kelola_data_user.php">Data User</a>
                             </nav>
                         </div>
 
